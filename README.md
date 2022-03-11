0e3a518ff2bd734fdfcd4b7910ed6db1fbf14b3a
Chih-Hao Lai <junesnow39@gmail.com>
2022-03-11 21:31:49 +0800

    add new hero koopa

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Gengar Image Ref: https://www.deviantart.com/xxlpanda/art/Gengar-Sprite-79533964
 ## Features  
 * Enter sleep mode if no button pressed in 10 seconds  
 * Press button to adjust screen brightness(PWM)  
+* two heroes with different difficulty
 
 ## Board  
 TTGO T-Display V1.1  
@@ -23,9 +24,9 @@ I reviced it to fit 803040 1000mAh battery.
 ![image](https://github.com/Chihhao/TTGO_GengarGame/blob/main/image/image0.jpg)  
 ![image](https://github.com/Chihhao/TTGO_GengarGame/blob/main/image/image1.jpg)  
 ![image](https://github.com/Chihhao/TTGO_GengarGame/blob/main/image/image2.jpg)  
-![image](https://github.com/Chihhao/TTGO_GengarGame/blob/main/image/image3.jpg)  
-![image](https://github.com/Chihhao/TTGO_GengarGame/blob/main/image/image4.png)  
+![image](https://github.com/Chihhao/TTGO_GengarGame/blob/main/image/image3.png)  
 
 ## Image to C Code  
 * use lcd-image-converter  
+* https://thesolaruniverse.wordpress.com/2021/11/05/displaying-color-pictures-on-a-240x240-tft-screen-with-st7789-controller-with-an-esp32-wroom-32/
   
--- TTGO_GengarGame/TTGO_GengarGame.ino
+++ TTGO_GengarGame/TTGO_GengarGame.ino
@@ -5,10 +5,11 @@
 // TFT_eSPI Library Version: 2.4.32
 // Button2 Library Version: 1.4.0
 
-#include <TFT_eSPI.h>   
-#include "initPicture.h" 
-#include "Button2.h";
+#include <TFT_eSPI.h>
+#include "Button2.h"
 #include "item.h"
+#include "gengar.h"
+#include "koopa.h"
 
 #define STATUS_GAMEOVER 0
 #define STATUS_RUN 1
@@ -16,14 +17,16 @@
 
 TFT_eSPI    tft = TFT_eSPI();        
 TFT_eSprite spriteScreen = TFT_eSprite(&tft);
-TFT_eSprite spriteDino = TFT_eSprite(&tft);
+TFT_eSprite spriteHero = TFT_eSprite(&tft);
+TFT_eSprite spriteHero1 = TFT_eSprite(&tft);
+TFT_eSprite spriteHero2 = TFT_eSprite(&tft);
 TFT_eSprite spriteEnemy0 = TFT_eSprite(&tft);
 TFT_eSprite spriteEnemy1 = TFT_eSprite(&tft);
 TFT_eSprite spriteGameOver = TFT_eSprite(&tft);
-
-#define BUTTON_A_PIN  0    //按鍵A
-#define BUTTON_B_PIN  35   //按鍵B
-#define TIME_TO_SLEEP 12000  //12S
+  
+#define BUTTON_A_PIN  0    
+#define BUTTON_B_PIN  35  
+#define TIME_TO_SLEEP 10000  //10S
 Button2 buttonA = Button2(BUTTON_A_PIN);
 Button2 buttonB = Button2(BUTTON_B_PIN);
 
@@ -31,30 +34,26 @@ const int pwmFreq = 5000;
 const int pwmResolution = 8;
 const int pwmLedChannelTFT = 0;  
 
-//const int DINO_W=33, DINO_H=35;
-const int DINO_W=32, DINO_H=42;
-const int ENEMY_W=18, ENEMY_H=38;
-
-const float INITIAL_SPEED = 1.0;
+float initialSpeed;
 
 float linesX[6];
 int linesW[6];
 float linesX2[6];
 int linesW2[6];
-float clouds[2]={random(0,80),random(100,180)};
-float bumps[2];
-int bumpsF[2];
+float cloudX[2], cloudY[2];
+float bumpsX[2];
+int bumpType[2];
 
-float eX[2];
-int eColor[2];
+float enemyX[2];
+int enemyColor[2];
 
 float fSpeed;
-int dinoX=30;
-int dinoY=58;
-float jumpDir=-1.4;
+int hero_Y;
+float jumpSpeed;
 
 int frames=0;
-int dino_pose=0;
+
+int bump_no;
 float cloudfSpeed=0.4;
 int gameStatus;
 int score=0;
@@ -68,39 +67,65 @@ int brightness=1;
 long last_available_time = 0;
 long game_start_time = 0;
 
+int JUMP_TOP, JUMP_BOTTOM;
+
+int hero_W, hero_H;
+int heroSelectedIdx = 0;
+int heroType=0;
+
+int hero_front_Type=1;
+
 void initGame(){
-  gameStatus = STATUS_INIT;  
+  if(heroSelectedIdx == 0){
+    hero_W = KOOPA_W;
+    hero_H = KOOPA_H;  
+    jumpSpeed = -1.8;  
+    initialSpeed = 1.0;
+  }
+  else if(heroSelectedIdx == 1){  
+    hero_W = GENGAR_W;
+    hero_H = GENGAR_H;  
+    jumpSpeed = -2.2;  
+    initialSpeed = 1.2;
+  }
+  JUMP_TOP = GROUND_Y - ENEMY_H - hero_H - 10;
+  JUMP_BOTTOM = GROUND_Y + 14 - hero_H;
   
-  clouds[0] = random(0,80);
-  clouds[1] = random(100,180);
+  hero_Y = JUMP_BOTTOM;
+  fSpeed = initialSpeed; 
   
-  eX[0] = random(240,310);
-  eX[1] = random(400,470);
-  eColor[0] = RandomColor();
-  eColor[1] = RandomColor();
+  cloudX[0] = random(0, 80);
+  cloudX[1] = random(100, 180);
+  cloudY[0] = random(20, 40);
+  cloudY[1] = random(20, 40);
   
-  fSpeed=INITIAL_SPEED; 
-  dinoX=30;
-  dinoY=58;
-  jumpDir=-1.8;  //ori:-1.4
-  frames=0;
-  dino_pose=0;
-  cloudfSpeed=0.4;
-  score=0;
+  enemyX[0] = random(240, 310);
+  enemyX[1] = random(400, 470);
+  enemyColor[0] = RandomColor();
+  enemyColor[1] = RandomColor();
+    
+  frames = 0;
+  heroType = 0;
+  bump_no = random(0, 2);
+  cloudfSpeed = 0.4;
+  score = 0;
 
   for(int i=0; i<6; i++){
-    linesX[i]=random(i*40,(i+1)*40);
-    linesW[i]=random(1,14);
-    linesX2[i]=random(i*40,(i+1)*40);
-    linesW2[i]=random(1,14);
+    linesX[i] = random(i*40, (i+1)*40);
+    linesW[i] = random(1, 14);
+    linesX2[i] = random(i*40, (i+1)*40);
+    linesW2[i] = random(1, 14);
   }
 
   for(int n=0; n<2; n++){
-    bumps[n]=random(n*90,(n+1)*120);
-    bumpsF[n]=random(0,2);
+    bumpsX[n] = random(n*90, (n+1)*120);
+    bumpType[n] = random(0, 2); //0 or 1
   }
 
-  tft.pushImage(0, 0, 240, 135, initPicture); 
+  spriteHero.createSprite(hero_W, hero_H);
+  spriteEnemy0.createSprite(ENEMY_W, ENEMY_H);
+  spriteEnemy1.createSprite(ENEMY_W, ENEMY_H);
+
 }
 
 void pressA(Button2& btn) {
@@ -108,26 +133,36 @@ void pressA(Button2& btn) {
   
   if(gameStatus == STATUS_RUN){
     if(!jumped) {
-      jumped=true;
-      dino_pose=0;  
+      jumped = true;
+      heroType=0;  
     }
   }
   else if(gameStatus == STATUS_GAMEOVER){
-    initGame();    
+    gameStatus = STATUS_INIT;
   }
   else if(gameStatus == STATUS_INIT){
     last_available_time = millis();
     game_start_time = millis();
-    tft.fillScreen(TFT_BLACK);    
+    tft.fillScreen(TFT_BLACK);
+    initGame();
     gameStatus = STATUS_RUN;
   }
   
 }
 
-void pressB(Button2& btn) {
+void pressB(Button2& btn) {  
   last_available_time = millis();
-  if(brightness++ > BRIGHTNESS_LEVEL - 1) { brightness=0; }  
-  ledcWrite(pwmLedChannelTFT, brightness_level[brightness]); 
+
+  if(gameStatus == STATUS_INIT){    
+    if(heroSelectedIdx==0) heroSelectedIdx = 1;
+    else if(heroSelectedIdx==1) heroSelectedIdx = 0;
+  }
+  else{
+    if(brightness++ > BRIGHTNESS_LEVEL - 1) { 
+      brightness=0; 
+    }  
+    ledcWrite(pwmLedChannelTFT, brightness_level[brightness]); 
+  }
 }
 
 void setup() {   
@@ -141,27 +176,20 @@ void setup() {
   buttonB.setPressedHandler(pressB);
   
   tft.init();    
-  tft.setSwapBytes(true);
+  //tft.setSwapBytes(true);
+  tft.setSwapBytes(false);
+  
   tft.fillScreen(TFT_BLACK);
   tft.setRotation(1);
-
-//  spriteDino.setColorDepth(16);
-//  spriteScreen.setColorDepth(16);
-    
-  spriteScreen.createSprite(240, 100);
-  spriteDino.createSprite(DINO_W, DINO_H);
-  spriteEnemy0.createSprite(ENEMY_W, ENEMY_H);  //int ENEMY_W=18, ENEMY_H=38;
-  spriteEnemy1.createSprite(ENEMY_W, ENEMY_H);
-
-  //spriteScreen.setTextColor(TFT_WHITE);  
-
-
-    
+  
   ledcSetup(pwmLedChannelTFT, pwmFreq, pwmResolution);
   ledcAttachPin(TFT_BL, pwmLedChannelTFT);
   ledcWrite(pwmLedChannelTFT, brightness_level[brightness]);
 
-  initGame();
+  spriteScreen.createSprite(240, 135);
+  spriteScreen.setTextColor(TFT_YELLOW);
+
+  gameStatus = STATUS_INIT;
 }
 
 void loop() {
@@ -176,14 +204,20 @@ void loop() {
   
   if(gameStatus == STATUS_RUN){
     if(jumped){ 
-      dinoY = dinoY + jumpDir; 
-      if(dinoY<=2)  { jumpDir = -jumpDir; }
-      if(dinoY>=58) { jumpDir = -jumpDir; jumped = false;}
+      hero_Y += jumpSpeed; 
+      if(hero_Y <= JUMP_TOP)  { 
+        jumpSpeed = -jumpSpeed; 
+      }
+      else if(hero_Y >= JUMP_BOTTOM) {
+        hero_Y = JUMP_BOTTOM;
+        jumpSpeed = -jumpSpeed; 
+        jumped = false;
+      }
     }
-    if(frames<8 && !jumped) dino_pose=1;
-    if(frames>8 && !jumped) dino_pose=2;
+    if(frames < 8 && !jumped) heroType = 1;
+    if(frames > 8 && !jumped) heroType = 2;
   
-    drawS(dino_pose);
+    drawS(heroType);
     frames++;
     if(frames==16) frames=0;
  
@@ -191,81 +225,93 @@ void loop() {
   }
 
   if(gameStatus == STATUS_GAMEOVER){
-    spriteGameOver.createSprite(240, 135);
-    spriteGameOver.fillSprite(TFT_BLACK);
-    spriteGameOver.setTextColor(TFT_YELLOW);
-    spriteGameOver.setTextSize(2); 
-    spriteGameOver.drawString("GAME OVER", 25, 25, 2);  
-    spriteGameOver.drawString("Score: " + String(score), 25, 75, 2);      
-    spriteGameOver.pushSprite(0, 0);
-    spriteGameOver.deleteSprite();
+    spriteScreen.fillSprite(TFT_BLACK);    
+    spriteScreen.setTextSize(2); 
+    spriteScreen.drawString("GAME OVER", 25, 25, 2);  
+    spriteScreen.drawString("Score: " + String(score), 25, 75, 2);      
+    spriteScreen.pushSprite(0, 0);
+
+    spriteHero.deleteSprite();
+    spriteEnemy0.deleteSprite();
+    spriteEnemy1.deleteSprite();    
+  }   
+
+  if(gameStatus == STATUS_INIT){
+    showHeroSelection();   
   }
-   
+
 }
 
-void drawS(int dino_pose){ 
+void drawS(int heroType){ 
   spriteScreen.fillSprite(TFT_BLACK);
-  spriteScreen.drawLine(0, 84, 240, 84, TFT_WHITE);
+  spriteScreen.drawLine(0, GROUND_Y, 240, GROUND_Y, TFT_WHITE);
   
   for(int i=0; i<6; i++){
-    spriteScreen.drawLine(linesX[i],87 ,linesX[i]+linesW[i], 87, TFT_WHITE);
-    linesX[i]=linesX[i]-fSpeed;
-    if(linesX[i]<-14){
-      linesX[i]=random(245,280);
-      linesW[i]=random(1,14);
+    spriteScreen.drawLine(linesX[i], GROUND_Y+3 ,linesX[i]+linesW[i], GROUND_Y+3, TFT_WHITE);
+    linesX[i] = linesX[i] - fSpeed;
+    if(linesX[i] < -14){
+      linesX[i]=random(245, 280);
+      linesW[i]=random(1, 14);
     }
-    spriteScreen.drawLine(linesX2[i],98 ,linesX2[i]+linesW2[i], 98, TFT_WHITE);
-    linesX2[i]=linesX2[i]-fSpeed;
-    if(linesX2[i]<-14){
-      linesX2[i]=random(245,280);
-      linesW2[i]=random(1,14);
-    }
-  }
-  
-  for(int j=0; j<2; j++){
-    spriteScreen.drawXBitmap(clouds[j], 20, cloud, 38, 11, TFT_WHITE, TFT_BLACK);
-    clouds[j]=clouds[j]-cloudfSpeed;
-    if(clouds[j]<-40){
-      clouds[j]=random(244,364);
+    spriteScreen.drawLine(linesX2[i], GROUND_Y+14 , linesX2[i]+linesW2[i], GROUND_Y+14, TFT_WHITE);
+    linesX2[i] = linesX2[i] - fSpeed;
+    if(linesX2[i] < -14){
+      linesX2[i]=random(245, 280);
+      linesW2[i]=random(1, 14);
     }
   }
 
+  // Draw Cloud
+  for(int j=0; j<2; j++){
+    spriteScreen.drawXBitmap(cloudX[j], cloudY[j], cloud, 38, 11, TFT_WHITE, TFT_BLACK);
+    cloudX[j] = cloudX[j] - cloudfSpeed;
+    if(cloudX[j] < -40){
+      cloudX[j] = random(244, 364);
+      cloudY[j] = random(20, 40);
+    }
+  }
+
+  // Draw Bump
   for(int n=0; n<2; n++){
-    spriteScreen.drawXBitmap(bumps[n], 80, bump[bumpsF[n]], 34, 5, TFT_WHITE, TFT_BLACK);
-    bumps[n]=bumps[n]-fSpeed;
-    if(bumps[n]<-40){
-      bumps[n]=random(244,364);
-      bumpsF[n]=random(0,2);
+    spriteScreen.drawXBitmap(bumpsX[n], GROUND_Y-4, bump[bumpType[n]], 34, 5, TFT_WHITE, TFT_BLACK);
+    bumpsX[n] = bumpsX[n] - fSpeed;
+    if(bumpsX[n] < -40){
+      bumpsX[n] = random(244, 364);
+      bumpType[n] = random(0, 2); //0 or 1
     }
   }
-  
+
+  // Move Enemy or Create New Enemy
   for(int enemyNo=0; enemyNo<2; enemyNo++){
-    eX[enemyNo] -= fSpeed;
-    if(eX[enemyNo] < -20) {
-      do{
-        eX[enemyNo] = random(240,400); //產生新敵人
-      }while(abs(eX[0]-eX[1])< 40+40*fSpeed);     
-      eColor[enemyNo] = RandomColor();
+    enemyX[enemyNo] -= fSpeed;
+    if(enemyX[enemyNo] < -20) {
+      enemyX[enemyNo] = random(240,360) + 60 * fSpeed; //Create New Enemy
+//      do{
+//        enemyX[enemyNo] = random(240,480); //Create New Enemy
+//      }while(fabs(enemyX[0]-enemyX[1]) < 40+hero_W*fSpeed);     
+      enemyColor[enemyNo] = RandomColor();
     }
   }
   
-  spriteEnemy0.drawXBitmap(0, 0, enemy[0], ENEMY_W, ENEMY_H, eColor[0], TFT_BLACK);
-  spriteEnemy1.drawXBitmap(0, 0, enemy[1], ENEMY_W, ENEMY_H, eColor[1], TFT_BLACK);  
-  //spriteDino.drawXBitmap(0, 0, dino[dino_pose], DINO_W, DINO_H, TFT_WHITE, TFT_BLACK);
-  //spriteDino.drawXBitmap(0, 0, dino, DINO_W, DINO_H, TFT_PURPLE);
-  spriteDino.pushImage(0, 0, DINO_W, DINO_H, dino[dino_pose], TFT_GREEN);  
+  spriteEnemy0.drawXBitmap(0, 0, enemy[0], ENEMY_W, ENEMY_H, enemyColor[0], TFT_BLACK);
+  spriteEnemy1.drawXBitmap(0, 0, enemy[1], ENEMY_W, ENEMY_H, enemyColor[1], TFT_BLACK);
+  if(heroSelectedIdx==0){
+    spriteHero.pushImage(0, 0, hero_W, hero_H, koopa[heroType]); 
+  }
+  else if(heroSelectedIdx==1){    
+    spriteHero.pushImage(0, 0, hero_W, hero_H, gengar[heroType]);  
+  }  
     
-  spriteEnemy0.pushToSprite(&spriteScreen, eX[0], 56, TFT_BLACK);
-  spriteEnemy1.pushToSprite(&spriteScreen, eX[1], 56, TFT_BLACK);
-  //spriteDino.pushToSprite(&spriteScreen, dinoX, dinoY, TFT_BLACK);
-  spriteDino.pushToSprite(&spriteScreen, dinoX, dinoY, TFT_GREEN);
+  spriteEnemy0.pushToSprite(&spriteScreen, enemyX[0], ENEMY_Y, TFT_BLACK);
+  spriteEnemy1.pushToSprite(&spriteScreen, enemyX[1], ENEMY_Y, TFT_BLACK);
+  spriteHero.pushToSprite(&spriteScreen, HERO_X, hero_Y, TFT_GREEN);
 
   score=(millis()-game_start_time)/120;
-  spriteScreen.drawString(String(score), 204, 0, 2);
-  spriteScreen.pushSprite(0, 17);
+  spriteScreen.setTextSize(1); 
+  spriteScreen.drawString(String(score), 200, 12, 2);
+  spriteScreen.pushSprite(0, 0);
 
-  fSpeed = INITIAL_SPEED + (score/100)*0.1;
-  
+  fSpeed = initialSpeed + (score/100)*0.1;  
 }
 
 int RandomColor(){
@@ -277,9 +323,9 @@ int RandomColor(){
 
 void checkColision(){
   for(int enemyNo=0; enemyNo<2; enemyNo++){
-    if( eX[enemyNo] < dinoX+DINO_W/2 && 
-        eX[enemyNo] > dinoX && 
-        dinoY > 25){
+    if( enemyX[enemyNo] < HERO_X + hero_W/2 && 
+        enemyX[enemyNo] > HERO_X && 
+        hero_Y > JUMP_BOTTOM - ENEMY_H + 5){
       gameStatus = STATUS_GAMEOVER;
     }
   }
@@ -293,4 +339,58 @@ void doSleep(){
     esp_deep_sleep_start();
 }
 
- 
+void showHeroSelection(){
+  spriteScreen.fillSprite(TFT_BLACK);   
+  
+  // Draw Rect
+  if(heroSelectedIdx==0){
+    spriteScreen.drawRect(30, 27, 80, 80, TFT_YELLOW);  
+    spriteScreen.drawRect(130, 27, 80, 80, TFT_DARKGREY);
+  }
+  else{
+    spriteScreen.drawRect(30, 27, 80, 80, TFT_DARKGREY);  
+    spriteScreen.drawRect(130, 27, 80, 80, TFT_YELLOW);
+  }
+
+  spriteHero1.createSprite(KOOPA_FRONT_W, KOOPA_FRONT_H);
+  spriteHero2.createSprite(GENGAR_FRONT_W, GENGAR_FRONT_H);
+
+  int X0,Y0;
+  X0 = 70 - KOOPA_FRONT_W/2;
+  Y0 = 95 - KOOPA_FRONT_H + 5;
+  int X1,Y1;
+  X1 = 170 - GENGAR_FRONT_W/2;
+  Y1 = 95  - GENGAR_FRONT_H - 5;
+    
+  // Draw Heros
+  if(heroSelectedIdx == 0){ //select koopa
+    // Hero 0 koopa walks
+    spriteHero1.pushImage(0, 0, KOOPA_FRONT_W, KOOPA_FRONT_H, koopa_front[hero_front_Type]);
+    spriteHero1.pushToSprite(&spriteScreen, X0, Y0, TFT_GREEN);
+
+    // Hero 1 gengar Stop 
+	  spriteHero2.pushImage(0, 0, GENGAR_FRONT_W, GENGAR_FRONT_H, gengar_front[0]);
+    spriteHero2.pushToSprite(&spriteScreen, X1, Y1/*, TFT_BLACK*/);
+    
+  }
+  else if(heroSelectedIdx == 1){
+    // Hero 0 koopa Stop
+	  spriteHero1.pushImage(0, 0, KOOPA_FRONT_W, KOOPA_FRONT_H, koopa_front[0]);
+    spriteHero1.pushToSprite(&spriteScreen, X0, Y0, TFT_GREEN);
+
+    // Hero 1 gengar walks
+    spriteHero2.pushImage(0, 0, GENGAR_FRONT_W, GENGAR_FRONT_H, gengar_front[hero_front_Type]);
+    spriteHero2.pushToSprite(&spriteScreen, X1, Y1/*, TFT_BLACK*/);
+  }
+
+  if(frames < 8 && !jumped) hero_front_Type = 1;
+  if(frames > 8 && !jumped) hero_front_Type = 2;
+
+  frames++;
+  if(frames==16) frames=0;
+  
+  spriteScreen.pushSprite(0, 0); 
+   
+  spriteHero1.deleteSprite();
+  spriteHero2.deleteSprite();  
+}
